a096b7bf71073f0652be2e0247ae022eb43fb872
lastresmoya <lastresmoyallc@gmail.com>
2018-11-26 11:38:09 -0600

    game features

--- src/js/pages/ProductPage/ProductFeatures/index.js
+++ src/js/pages/ProductPage/ProductFeatures/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 
 function ProductFeatures (props) {
     const features = props.features;
+    console.log(features)
     return(
         <section className="mb-5">
             <ul className="list-unstyled">
                 <li className="lead">GAME FEATURES</li>
-                <li className="my-3">Icon Single Player</li>
-                <li className="my-3">Icon Achievements</li>
-                <li className="my-3">Icon Controller Support</li>
-                <li className="my-3">Icon Cloud Saves</li>
-                <li className="my-3">Icon Via Steam</li>
+                <li className="my-3">{features.multiplayer ? 'multiplayer' : 'Single player'}</li>
+                <li className="my-3">{features.achievements ? 'Achievements' : ''}</li>
+                <li className="my-3">{features.controller ? 'Controller Support' : ''}</li>
+                <li className="my-3">{features.cloud ? 'Cloud Saves' : ''}</li>
+                <li className="my-3">{features.cloud ? 'Via Steam' : ''}</li>
             </ul>
         </section>
     )
